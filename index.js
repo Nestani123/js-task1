58b554a4ac2d884b0281bf15302bf41b88c3406f
Nestani123 <nestangagadze96@gmail.com>
2024-02-16 22:46:21 +0400

    second part is also done

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+//exercise1
 for(var index=5;index>0;index--){
     console.log(index);
+}
+
+//exercise2
+console.log("This is just to make a split between those two tasks");
+
+for(var index=0; index<10;index++){
+    if(index % 2 != 0){
+    console.log(index);
+    }
 }
